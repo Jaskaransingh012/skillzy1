83a6a745f6b2f095612609287cac901339153da8
Jaskaransingh012 <jaskaransingh_1@outlook.com>
2025-02-25 12:16:52 +0530

    hello

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 // Register User
@@ -12,11 +12,9 @@ exports.register = async (req, res) => {
     if (user) return res.status(400).json({ message: 'User already exists' });
   
     // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
+   
     // Create new user
-    user = new User({ username, email, password: hashedPassword, role });
+    user = new User({ username, email, password, role });
     await user.save();
 
     res.status(201).json({ message: 'User registered successfully' });
